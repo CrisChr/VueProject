b7c152894501022d7d84f0a21795ca0b59d1c32d
Hongru Cai <cdcaihr@cn.ibm.com>
2018-09-21 15:47:25 +0800

    Add my blog

--- simple_vue/src/components/MyBlog.vue
+++ simple_vue/src/components/MyBlog.vue
@@ -1,5 +1,19 @@
 <template>
-  <el-button type="danger" icon="el-icon-edit" circle class="edit" @click="CreateBlog"></el-button>
+  <el-col :span="20">
+    <el-button type="danger" icon="el-icon-edit" circle class="edit" @click="CreateBlog"></el-button>
+    <el-table style="margin-top:50px" :data="tableData">
+      <el-table-column prop="date" label="Created"></el-table-column>
+      <el-table-column prop="title" label="Title"></el-table-column>
+      <el-table-column prop="operate" label="Operation" fixed="right">
+        <template slot-scope="scope">
+          <el-button type="text" size="small">view</el-button>
+          <el-button type="text" size="small">update</el-button>
+          <el-button type="text" size="small">remove</el-button>
+        </template>
+      </el-table-column>
+      
+    </el-table>
+  </el-col>
 </template>
 
 <script>
@@ -9,7 +23,8 @@ export default {
   name: "myblog",
   data() {
     return {
-      msg: "My Blog"
+      msg: "My Blog",
+      tableData: []
     };
   },
   mounted: function() {
@@ -20,6 +35,20 @@ export default {
         message: 'Please log in'
       })
       this.$router.push('/account/login/myblog')
+    }else{
+      let user = this.$localStorage.get("user")
+      let blog = this.$localStorage.get('blog')
+      if(typeof blog === 'string'){
+        blog = JSON.parse(blog)
+      }
+      console.log("blog type: "+typeof blog)
+      if(blog != null){
+        for(let i=0; i<blog.length; i++){
+          if(user.name === blog[i].username){
+            this.tableData.push(blog[i])
+          }
+        }
+      }
     }
   },
   methods: {
